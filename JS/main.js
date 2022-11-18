4d2f4913b175eba05f79e236df10978e70cb8bbc
Englajanze <engla.janze@icloud.com>
2022-11-18 09:55:42 +0700

    Divided in to divs

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,4 @@ import Model from "./Model";
 import Controller from "./Controller";
 import View from "./View";
 
-const app = new Controller(new Model(), new View());
+// const app = new Controller(new Model(), new View());
